380d124abc4c3ce2c77c15185b886245a43d5863
jomatheux <jomatheux@gmail.com>
2024-08-19 00:06:23 -0300

    Department ComboBox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -106,6 +106,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IOException", "Error load view", e.getMessage(), AlertType.ERROR);
 		}
 	}
